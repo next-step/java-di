8d8fd4016db18f5f38b5ef9286f3c23d2f1936d1
jinyoung <jinyoungchoi95@gmail.com>
2024-07-23 22:07:32 +0900

    test: Lifecycle 학습테스트 추가

--- study/src/main/java/lifecycle/LifecycleConfig.java
+++ study/src/main/java/lifecycle/LifecycleConfig.java
@@ -18,7 +18,7 @@ public class LifecycleConfig {
 
   // Bean 대상 객체의 메서드명에 close 또는 shutdown가 있으면 소멸 메서드를 자동으로 유추합니다.
   // PojoSampleObject 클래스의 close() 메서드를 감지하여 destroyMethod를 자동으로 설정합니다.
-  @Bean(initMethod = "init") //, destroyMethod = "close")
+  @Bean(initMethod = "init", destroyMethod = "close")
   public PojoSampleObject pojoSampleObject() {
     return new PojoSampleObject();
   }
