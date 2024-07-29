7e889a4b4172089b3c374c52735394e94134ca51
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-30 02:42:19 +0900

    fix(DefaultListableBeanFactory): di 테스트 통과하도록 수정

--- di/src/main/java/com/interface21/beans/factory/support/DefaultListableBeanFactory.java
+++ di/src/main/java/com/interface21/beans/factory/support/DefaultListableBeanFactory.java
@@ -1,36 +1,87 @@
 package com.interface21.beans.factory.support;
 
+import com.interface21.beans.BeanInstantiationException;
 import com.interface21.beans.factory.BeanFactory;
 import com.interface21.beans.factory.config.BeanDefinition;
+import com.interface21.beans.factory.config.SimpleBeanDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DefaultListableBeanFactory implements BeanFactory {
 
     private static final Logger log = LoggerFactory.getLogger(DefaultListableBeanFactory.class);
 
-    private final Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
+    private final Map<Class<?>, BeanDefinition> beanDefinitionMap;
 
-    private final Map<Class<?>, Object> singletonObjects = new HashMap<>();
+    private final Map<Class<?>, Object> singletonObjects;
+
+    public DefaultListableBeanFactory() {
+        final BeanScanner beanScanner = new BeanScanner("samples");
+        beanDefinitionMap = beanScanner.scan()
+                .stream()
+                .map(SimpleBeanDefinition::new)
+                .collect(Collectors.toMap(
+                        SimpleBeanDefinition::getType,
+                        Function.identity()
+                ));
+        singletonObjects = new HashMap<>();
+    }
 
     @Override
     public Set<Class<?>> getBeanClasses() {
-        return Set.of();
+        return beanDefinitionMap.keySet();
     }
 
     @Override
     public <T> T getBean(final Class<T> clazz) {
-        return null;
+        return clazz.cast(singletonObjects.get(clazz));
     }
 
     public void initialize() {
+        getBeanClasses().forEach(this::initBean);
+    }
+
+    private Object initBean(final Class<?> beanClass) {
+        final Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(beanClass, getBeanClasses())
+                .orElseThrow(() -> new BeanInstantiationException(beanClass, "Could not autowire. No concrete class found for %s.".formatted(beanClass.getName())));
+        final Object createdBean = createBean(beanDefinitionMap.get(concreteClass).getConstructor());
+        singletonObjects.put(beanClass, createdBean);
+        return createdBean;
+    }
+
+    private Object createBean(final Constructor<?> constructor) {
+        try {
+            return constructor.newInstance(createConstructorArgs(constructor));
+        } catch (final InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new BeanInstantiationException(constructor.getDeclaringClass(), e.getMessage(), e);
+        }
+    }
+
+    private Object[] createConstructorArgs(final Constructor<?> constructor) {
+        return Stream.of(constructor.getParameterTypes())
+                .map(this::getOrCreateBean)
+                .toArray();
+    }
+
+    private Object getOrCreateBean(final Class<?> parameterType) {
+        if (singletonObjects.containsKey(parameterType)) {
+            return singletonObjects.get(parameterType);
+        }
+        return initBean(parameterType);
     }
 
     @Override
     public void clear() {
+        beanDefinitionMap.clear();
+        singletonObjects.clear();
     }
 }
--- di/src/test/java/com/interface21/beans/factory/support/DefaultListableBeanFactoryTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/DefaultListableBeanFactoryTest.java
@@ -1,14 +1,17 @@
 package com.interface21.beans.factory.support;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import samples.JdbcSampleRepository;
 import samples.SampleController;
+import samples.SampleService;
 
-import java.lang.annotation.Annotation;
-import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DefaultListableBeanFactoryTest {
@@ -35,12 +38,22 @@ class DefaultListableBeanFactoryTest {
         assertNotNull(sampleService.getSampleRepository());
     }
 
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = new HashSet<>();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        return beans;
+    @Test
+    @DisplayName("bean 들의 모든 class 정보를 가져올 수 있다.")
+    void getBeanClassesTest() {
+        final Set<Class<?>> beanClasses = beanFactory.getBeanClasses();
+
+        assertThat(beanClasses).containsExactlyInAnyOrder(SampleController.class, SampleService.class, JdbcSampleRepository.class);
+    }
+
+    @Test
+    @DisplayName("clear 시 모든 정보가 지워진다.")
+    void clearTest() {
+        beanFactory.clear();
+
+        assertSoftly(softly -> {
+            softly.assertThat(beanFactory.getBeanClasses()).isEmpty();
+            softly.assertThat(beanFactory.getBean(SampleController.class)).isNull();
+        });
     }
 }
