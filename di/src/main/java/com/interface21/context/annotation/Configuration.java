2bd3e7e2d9422917dd497a504d385826ec4d84e5
jinyoung <jinyoungchoi95@gmail.com>
2024-08-02 19:52:52 +0900

    feat: remove component

--- di/src/main/java/com/interface21/context/annotation/Configuration.java
+++ di/src/main/java/com/interface21/context/annotation/Configuration.java
@@ -1,7 +1,5 @@
 package com.interface21.context.annotation;
 
-import com.interface21.context.stereotype.Component;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,7 +7,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Component
 public @interface Configuration {
 
 }
