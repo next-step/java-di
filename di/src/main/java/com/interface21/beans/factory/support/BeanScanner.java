2c94d708997778dc1d8c37fdf06119ed6be9bd31
SeolYoungKim <nasur4da@gmail.com>
2024-08-03 16:12:15 +0900

    feat(BeanScanner): 빈스캐너에 @Configuration이 달린 클래스를 스캔할 수 있도록 기능 구현

--- di/src/main/java/com/interface21/beans/factory/support/BeanScanner.java
+++ di/src/main/java/com/interface21/beans/factory/support/BeanScanner.java
@@ -1,6 +1,8 @@
 package com.interface21.beans.factory.support;
 
+import com.interface21.context.annotation.Configuration;
 import com.interface21.context.stereotype.Component;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -9,17 +11,20 @@ import org.reflections.Reflections;
 public class BeanScanner {
     private static final String STEREOTYPE_PACKAGE = "com.interface21.context.stereotype";
 
-    private final String[] basePackages;
+    private final Reflections reflections;
 
     public BeanScanner(String... basePackages) {
-        this.basePackages = basePackages;
+        this.reflections = new Reflections(STEREOTYPE_PACKAGE, basePackages);
     }
 
-    public Set<Class<?>> scan() {
-        Reflections reflections = new Reflections(STEREOTYPE_PACKAGE, basePackages);
+    public Set<Class<?>> scanComponent() {
         return reflections.getTypesAnnotatedWith(Component.class)
                 .stream()
                 .filter(Predicate.not(Class::isAnnotation))
                 .collect(Collectors.toSet());
+    }
+
+    public Set<Class<?>> scanConfiguration() {
+        return new HashSet<>(reflections.getTypesAnnotatedWith(Configuration.class));
     }
 }
