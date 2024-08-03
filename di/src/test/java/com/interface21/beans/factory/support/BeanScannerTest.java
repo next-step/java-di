06cef39e8fc5031321304f401c2caa824d79d6ab
SeolYoungKim <nasur4da@gmail.com>
2024-08-03 16:12:37 +0900

    test(BeanScanner): 빈스캐너에 @Configuration이 달린 클래스를 스캔하는 기능 테스트 코드 작성

--- di/src/test/java/com/interface21/beans/factory/support/BeanScannerTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/BeanScannerTest.java
@@ -1,24 +1,41 @@
 package com.interface21.beans.factory.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import samples.JdbcSampleDataSource;
+import samples.IntegrationConfig;
 import samples.JdbcSampleRepository;
 import samples.SampleComponent;
 import samples.SampleController;
 import samples.SampleService;
 
 class BeanScannerTest {
-    @DisplayName("@Component, @Repository, @Service, @Controller 가 달린 클래스들을 스캔한다.")
+    @DisplayName("scanComponent()는 @Component, @Repository, @Service, @Controller 가 달린 클래스들을 스캔한다.")
     @Test
-    void scan() {
+    void scanComponent() {
         BeanScanner beanScanner = new BeanScanner("samples");
 
-        Set<Class<?>> beanClasses = beanScanner.scan();
+        Set<Class<?>> beanClasses = beanScanner.scanComponent();
 
-        assertThat(beanClasses).contains(JdbcSampleDataSource.class, JdbcSampleRepository.class, SampleComponent.class, SampleController.class, SampleService.class);
+        assertAll(
+                () -> assertThat(beanClasses).contains(SampleComponent.class, SampleController.class, SampleService.class, JdbcSampleRepository.class),
+                () -> assertThat(beanClasses).doesNotContain(IntegrationConfig.class)
+        );
+    }
+
+    @DisplayName("scanConfiguration()은 @Configuration 이 달린 클래스들을 스캔한다.")
+    @Test
+    void scanConfiguration() {
+        BeanScanner beanScanner = new BeanScanner("samples");
+
+        Set<Class<?>> beanClasses = beanScanner.scanConfiguration();
+
+        assertAll(
+                () -> assertThat(beanClasses).containsOnly(IntegrationConfig.class),
+                () -> assertThat(beanClasses).doesNotContain(SampleComponent.class, SampleController.class, SampleService.class, JdbcSampleRepository.class)
+        );
     }
 }
