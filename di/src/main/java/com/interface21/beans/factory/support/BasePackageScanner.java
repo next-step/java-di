32485e38c5ff65f7bffe4aa35a12b3b8f7c10c86
SeolYoungKim <nasur4da@gmail.com>
2024-08-03 15:28:02 +0900

    refactor(BasePackageScanner): 메서드 추출

--- di/src/main/java/com/interface21/beans/factory/support/BasePackageScanner.java
+++ di/src/main/java/com/interface21/beans/factory/support/BasePackageScanner.java
@@ -3,43 +3,49 @@ package com.interface21.beans.factory.support;
 import com.interface21.context.annotation.ComponentScan;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Set;
 import java.util.stream.Stream;
 import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
 
 public class BasePackageScanner {
+    private static final String ALL_PACKAGE = "";
+
     public String[] scan() {
         Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .forPackage(""));
+                .forPackage(ALL_PACKAGE));
+        Set<Class<?>> typesAnnotatedWithComponentScan = reflections.getTypesAnnotatedWith(ComponentScan.class);
 
-        // basePackage 필드에 값이 없는 클래스는 해당 메서드가 선언된 패키지 스캔
-        List<String> basePackages1 = reflections.getTypesAnnotatedWith(ComponentScan.class)
-                .stream()
-                .filter(Predicate.not(Class::isAnnotation))
-                .filter(clazz -> {
-                    ComponentScan componentScan = clazz.getAnnotation(ComponentScan.class);
-                    return componentScan.value().length == 0 && componentScan.basePackages().length == 0;
-                })
+        List<String> classPackages = typesAnnotatedWithComponentScan.stream()
+                .filter(this::isNoDeclaredBasePackages)
                 .map(Class::getPackageName)
                 .toList();
 
-        // basePackages 필드에 값을 가진 클래스 처리
-        List<String> basePackages2 = reflections.getTypesAnnotatedWith(ComponentScan.class)
-                .stream()
-                .filter(Predicate.not(Class::isAnnotation))
-                .map(clazz -> clazz.getAnnotation(ComponentScan.class))
-                .filter(componentScan -> componentScan.value().length != 0 || componentScan.basePackages().length != 0)
-                .flatMap(componentScan -> {
-                    if (componentScan.value().length == 0) {
-                        return Arrays.stream(componentScan.basePackages());
-                    }
-
-                    return Arrays.stream(componentScan.value());
-                })
+        List<String> basePackages = typesAnnotatedWithComponentScan.stream()
+                .filter(this::isDeclaredBasePackages)
+                .flatMap(this::extractBasePackages)
                 .toList();
 
-        return Stream.concat(basePackages1.stream(), basePackages2.stream())
+        return Stream.concat(classPackages.stream(), basePackages.stream())
                 .toArray(String[]::new);
+    }
+
+    private boolean isNoDeclaredBasePackages(Class<?> clazz) {
+        ComponentScan componentScan = clazz.getAnnotation(ComponentScan.class);
+        return componentScan.value().length == 0 && componentScan.basePackages().length == 0;
+    }
+
+    private boolean isDeclaredBasePackages(Class<?> clazz) {
+        ComponentScan componentScan = clazz.getAnnotation(ComponentScan.class);
+        return componentScan.value().length != 0 || componentScan.basePackages().length != 0;
+    }
+
+    private Stream<String> extractBasePackages(Class<?> clazz) {
+        ComponentScan componentScan = clazz.getAnnotation(ComponentScan.class);
+        if (componentScan.value().length == 0) {
+            return Arrays.stream(componentScan.basePackages());
+        }
+
+        return Arrays.stream(componentScan.value());
     }
 }
