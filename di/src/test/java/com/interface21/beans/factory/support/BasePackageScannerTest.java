daeaa3b8eeef10bb47b6beb57c22230886184695
SeolYoungKim <nasur4da@gmail.com>
2024-08-04 23:41:17 +0900

    test(BasePackageScanner): 테스트 통합

--- di/src/test/java/com/interface21/beans/factory/support/BasePackageScannerTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/BasePackageScannerTest.java
@@ -4,25 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import samples.SampleScanConfig;
 
 class BasePackageScannerTest {
-    @DisplayName("@ComponentScan 에 basePackage가 지정되지 않으면 애노테이션이 선언된 클래스의 패키지를 basePackage로 한다.")
+    @DisplayName("""
+            아래 두 케이스에 대해 검증한다.
+            1. @ComponentScan 에 basePackage가 지정되지 않으면 애노테이션이 선언된 클래스의 패키지를 basePackage로 한다.
+            2. @ComponentScan에 basePacakge가 지정된 경우 지정된 패키지를 basePackage로 한다.
+            """)
     @Test
     void scan() {
-        BasePackageScanner basePackageScanner = new BasePackageScanner();
+        BasePackageScanner basePackageScanner = new BasePackageScanner(SampleScanConfig.class);
 
         String[] basePackages = basePackageScanner.scan();
 
-        assertThat(basePackages).contains("samples");
-    }
-
-    @DisplayName("@ComponentScan에 basePacakge가 지정된 경우 지정된 패키지를 basePackage로 한다.")
-    @Test
-    void scan2() {
-        BasePackageScanner basePackageScanner = new BasePackageScanner();
-
-        String[] basePackages = basePackageScanner.scan();
-
-        assertThat(basePackages).contains("samples.scantest", "samples.scantest2");
+        assertThat(basePackages).containsOnly("samples", "samples.scantest", "samples.scantest2");
     }
 }
