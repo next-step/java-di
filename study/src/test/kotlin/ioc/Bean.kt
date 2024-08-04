975d8558fffd16e2c6ca718a5ec422ed035bee76
SeolYoungKim <nasur4da@gmail.com>
2024-08-04 15:52:51 +0900

    study(Bean): 테스트 작성

--- study/src/test/kotlin/ioc/Bean.kt
+++ study/src/test/kotlin/ioc/Bean.kt
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *       XML File   ─┐
  *       Annotation ─┤─── BeanDefinition
  *       Java code  ─┘          │
- *                      BeanFactory
+ *                         BeanFactory
  *                              │
  *                             Bean
  *
@@ -47,9 +47,10 @@ class Bean : FreeSpec({
         스프링 IoC 컨테이너는 생성자를 사용하여 Bean을 인스턴스화 합니다.
         어떤 프레임워크는 특정 인터페이스를 구현 해야 하거나 표준이 아닌 방식으로 코딩을 강제하는 경우가 있습니다.
         하지만 스프링은 표준 자바를 지원하므로 추가적인 인터페이스를 구현할 필요가 없습니다.
+        - POJO도 빈으로 등록할 수 있다. 
         @see AppConfig#sampleObject()
         """ {
-            applicationContext.isBeanNameInUse("sampleObject") shouldBe false
+            applicationContext.isBeanNameInUse("sampleObject") shouldBe true
         }
 
         """
@@ -60,7 +61,7 @@ class Bean : FreeSpec({
         XML 파일로 구성 메타데이터를 작성한다면 factory-method 속성을 사용하여 팩터리 메서드를 지정할 수 있습니다.
         @see AppConfig#sampleFactoryObject()
         """ {
-            applicationContext.isBeanNameInUse("sampleFactoryObject") shouldBe false
+            applicationContext.isBeanNameInUse("sampleFactoryObject") shouldBe true
         }
     }
 
@@ -75,7 +76,7 @@ class Bean : FreeSpec({
     일관된 Bean 이름은 구성(Configuration)을 더 쉽게 읽고 이해할 수 있습니다.
     또한 Spring AOP를 사용할 때 이름으로 Advice를 적용할 때 많은 도움이 됩니다.
     """ {
-        applicationContext.isBeanNameInUse("sampleObject") shouldBe false
+        applicationContext.isBeanNameInUse("sampleObject") shouldBe true
     }
 
     """
@@ -87,7 +88,7 @@ class Bean : FreeSpec({
     Java 구성은 @Bean 어노테이션의 name 속성을 사용하여 Bean 별칭을 지정할 수 있습니다.
     @see AppConfig#sampleAlias()
     """ {
-        applicationContext.isBeanNameInUse("firstName") shouldBe false
-        applicationContext.isBeanNameInUse("secondName") shouldBe false
+        applicationContext.isBeanNameInUse("firstName") shouldBe true
+        applicationContext.isBeanNameInUse("secondName") shouldBe true
     }
 })
