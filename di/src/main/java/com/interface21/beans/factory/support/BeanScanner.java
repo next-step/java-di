2662118102cb16678f1c7f4e9c49cee1d4102d41
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-05 22:39:01 +0900

    feat : beanScanner로 controller, service, repository bean 스캔

---
+++ di/src/main/java/com/interface21/beans/factory/support/BeanScanner.java
@@ -0,0 +1,33 @@
+package com.interface21.beans.factory.support;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.context.stereotype.Repository;
+import com.interface21.context.stereotype.Service;
+import java.lang.annotation.Annotation;
+import java.util.Collection;
+import java.util.List;
+import org.reflections.Reflections;
+
+public class BeanScanner {
+
+    private final BeanDefinitionRegistry beanDefinitionRegistry;
+
+    private final List<Class<? extends Annotation>> beanClasses = List.of(
+        Controller.class,
+        Service.class,
+        Repository.class
+    );
+
+    public BeanScanner(BeanDefinitionRegistry beanDefinitionRegistry) {
+        this.beanDefinitionRegistry = beanDefinitionRegistry;
+    }
+
+    public void scan(String... basePackages) {
+        Reflections reflections = new Reflections(basePackages);
+        beanClasses.stream()
+            .map(reflections::getTypesAnnotatedWith)
+            .flatMap(Collection::stream)
+            .forEach(beanClass -> beanDefinitionRegistry.registerBeanDefinition(beanClass,
+                new GenericBeanDefinition(beanClass)));
+    }
+}
