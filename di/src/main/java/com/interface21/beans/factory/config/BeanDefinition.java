2b599e0f692dc4cfd1d61e069ef914b0d4078fc2
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-05 22:41:09 +0900

    feat : beanDefinition 필드 추가 및 구현

--- di/src/main/java/com/interface21/beans/factory/config/BeanDefinition.java
+++ di/src/main/java/com/interface21/beans/factory/config/BeanDefinition.java
@@ -1,8 +1,22 @@
 package com.interface21.beans.factory.config;
 
+import com.interface21.beans.factory.support.InjectMode;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
+
 public interface BeanDefinition {
 
     Class<?> getType();
 
     String getBeanClassName();
+
+    InjectMode getInjectMode();
+
+    Constructor<?> getConstructor();
+
+    Set<Method> getMethods();
+
+    Set<Field> getFields();
 }
