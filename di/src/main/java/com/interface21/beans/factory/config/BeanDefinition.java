ade8a3416d9f8a24a5907f331fe93807925309d8
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-05 23:37:27 +0900

    feat(BeanDefinition): createBean 메서드 추가

--- di/src/main/java/com/interface21/beans/factory/config/BeanDefinition.java
+++ di/src/main/java/com/interface21/beans/factory/config/BeanDefinition.java
@@ -1,5 +1,8 @@
 package com.interface21.beans.factory.config;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Function;
+
 public interface BeanDefinition {
 
     Class<?> getType();
@@ -9,4 +12,6 @@ public interface BeanDefinition {
     default boolean hasSameName(final BeanDefinition beanDefinition) {
         return getBeanClassName().equals(beanDefinition.getBeanClassName());
     }
+
+    Object createBean(Function<Class<?>, Object> beanSupplier) throws InvocationTargetException, IllegalAccessException, InstantiationException;
 }
