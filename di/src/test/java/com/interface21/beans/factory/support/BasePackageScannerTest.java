5bb60c4369bb96dddff28989a409cecda9bfd492
SeolYoungKim <nasur4da@gmail.com>
2024-08-05 21:04:36 +0900

    refactor(BasePackageScanner): basePackageScanner가 Reflection이 필요 없을 것 같아 제거

--- di/src/test/java/com/interface21/beans/factory/support/BasePackageScannerTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/BasePackageScannerTest.java
@@ -4,20 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import samples.MultiPackageScanConfig;
 import samples.SampleScanConfig;
 
 class BasePackageScannerTest {
-    @DisplayName("""
-            아래 두 케이스에 대해 검증한다.
-            1. @ComponentScan 에 basePackage가 지정되지 않으면 애노테이션이 선언된 클래스의 패키지를 basePackage로 한다.
-            2. @ComponentScan에 basePacakge가 지정된 경우 지정된 패키지를 basePackage로 한다.
-            """)
+    @DisplayName("@ComponentScan 에 basePackage가 지정되지 않으면 애노테이션이 선언된 클래스의 패키지를 basePackage로 한다.")
     @Test
-    void scan() {
+    void scan1() {
         BasePackageScanner basePackageScanner = new BasePackageScanner(SampleScanConfig.class);
 
         String[] basePackages = basePackageScanner.scan();
 
-        assertThat(basePackages).containsOnly("samples", "samples.scantest", "samples.scantest2");
+        assertThat(basePackages).containsOnly("samples");
+    }
+
+    @DisplayName("@ComponentScan에 basePacakge가 지정된 경우 지정된 패키지를 basePackage로 한다.")
+    @Test
+    void scan2() {
+        BasePackageScanner basePackageScanner = new BasePackageScanner(MultiPackageScanConfig.class);
+
+        String[] basePackages = basePackageScanner.scan();
+
+        assertThat(basePackages).containsOnly("samples.scantest", "samples.scantest2");
     }
 }
