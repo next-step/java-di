d6b531ee807c56a061a913e3e046d1c10ab6af2c
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-07 01:05:41 +0900

    feat(NewDefaultListableBeanFactory): NewBeanDefinitionRegistry 를 구현한 새로운 버전의 DefaultListableBeanFactory 추가

---
+++ di/src/main/java/com/interface21/beans/factory/support/NewDefaultListableBeanFactory.java
@@ -0,0 +1,95 @@
+package com.interface21.beans.factory.support;
+
+import com.interface21.beans.BeanCurrentlyInCreationException;
+import com.interface21.beans.BeanInstantiationException;
+import com.interface21.beans.factory.BeanFactory;
+import com.interface21.beans.factory.config.BeanDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NewDefaultListableBeanFactory implements NewBeanDefinitionRegistry, BeanFactory {
+
+    private static final Logger log = LoggerFactory.getLogger(NewDefaultListableBeanFactory.class);
+
+    private final BeanDefinitionRegistry beanDefinitionRegistry;
+    private final SimpleBeanFactory beanFactory;
+    private final Set<BeanDefinition> tempBeansInCreation;
+
+    public NewDefaultListableBeanFactory() {
+        this.beanDefinitionRegistry = new SimpleBeanDefinitionRegistry();
+        this.beanFactory = new SimpleBeanFactory();
+        this.tempBeansInCreation = new HashSet<>();
+    }
+
+    @Override
+    public Set<Class<?>> getBeanClasses() {
+        return beanDefinitionRegistry.getBeanClasses();
+    }
+
+    @Override
+    public <T> T getBean(final Class<T> clazz) {
+        return beanFactory.getBean(clazz);
+    }
+
+    @Override
+    public <T> T getBean(final String beanName) {
+        return beanFactory.getBean(beanName);
+    }
+
+    public void initialize() {
+        beanDefinitionRegistry.getBeanDefinitions()
+                .forEach((s, beanDefinition) -> initBean(beanDefinition));
+    }
+
+    private Object initBean(final BeanDefinition beanDefinition) {
+        if (tempBeansInCreation.contains(beanDefinition)) {
+            throw new BeanCurrentlyInCreationException(tempBeansInCreation);
+        }
+
+        final String beanClassName = beanDefinition.getBeanClassName();
+        if (beanFactory.containsBean(beanClassName)) {
+            return beanFactory.getBean(beanClassName);
+        }
+
+        tempBeansInCreation.add(beanDefinition);
+
+        final Object createdBean = createBean(beanDefinition);
+        beanFactory.addBean(beanClassName, createdBean);
+
+        tempBeansInCreation.remove(beanDefinition);
+        return createdBean;
+    }
+
+    private Object createBean(final BeanDefinition beanDefinition) {
+        try {
+            return beanDefinition.createBean(this::getOrCreateBean);
+        } catch (final InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new BeanInstantiationException(beanDefinition.getType(), e.getMessage(), e);
+        }
+    }
+
+    private Object getOrCreateBean(final Class<?> parameterType) {
+        final BeanDefinition beanDefinition = beanDefinitionRegistry.getBeanDefinition(parameterType);
+        final String beanClassName = beanDefinition.getBeanClassName();
+        if (beanFactory.containsBean(beanClassName)) {
+            return beanFactory.getBean(beanClassName);
+        }
+        return initBean(beanDefinition);
+    }
+
+    @Override
+    public void clear() {
+        beanDefinitionRegistry.clear();
+        beanFactory.clear();
+        tempBeansInCreation.clear();
+    }
+
+    @Override
+    public void registerBeanDefinition(final Class<?> clazz, final BeanDefinition beanDefinition) {
+        beanDefinitionRegistry.registerBeanDefinition(clazz, beanDefinition);
+    }
+}
