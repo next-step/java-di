e72a0b4d74cfd9fa90a3655d41091ceaac12ac2b
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-06 23:41:31 +0900

    feat : applicationContext를 통해 controller 빈을 찾아 handlerMapping 처리되도록 리펙토링

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,8 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.ApplicationContext;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.web.method.support.HandlerMethodArgumentResolver;
 import com.interface21.webmvc.servlet.mvc.HandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.support.HttpRequestArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.HttpResponseArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.ModelArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.PathVariableArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.RequestParamArgumentResolver;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,34 +26,70 @@ import java.util.Map;
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final List<HandlerMethodArgumentResolver> argumentResolvers = List.of(
+        new HttpRequestArgumentResolver(),
+        new HttpResponseArgumentResolver(),
+        new RequestParamArgumentResolver(),
+        new PathVariableArgumentResolver(),
+        new ModelArgumentResolver()
+    );
 
-    private final Object[] basePackage;
+    private final ApplicationContext context;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+
+    public AnnotationHandlerMapping(ApplicationContext context) {
         this.handlerExecutions = new HashMap<>();
+        this.context = context;
     }
 
+    @Override
     public void initialize() {
-        final var controllerScanner = new ControllerScanner();
-        handlerExecutions.putAll(controllerScanner.scan(basePackage));
+        context.getBeansWithAnnotation(Controller.class)
+            .forEach(this::processController);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    @Override
     public Object getHandler(final HttpServletRequest request) {
-        final var requestUri = request.getRequestURI();
-        final var requestMethod = RequestMethod.valueOf(request.getMethod().toUpperCase());
-        log.debug("requestUri : {}, requestMethod : {}", requestUri, requestMethod);
-        return getHandlerInternal(new HandlerKey(requestUri, requestMethod));
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.entrySet()
+            .stream()
+            .filter(entry -> entry.getKey().isMatch(handlerKey))
+            .findAny()
+            .map(Map.Entry::getValue)
+            .orElse(null);
     }
 
-    private HandlerExecution getHandlerInternal(final HandlerKey requestHandlerKey) {
-        for (HandlerKey handlerKey : handlerExecutions.keySet()) {
-            if (handlerKey.isMatch(requestHandlerKey)) {
-                return handlerExecutions.get(handlerKey);
-            }
-        }
-        return null;
+    private void processController(Class<?> controller, Object instance) {
+        Arrays.stream(controller.getMethods())
+            .filter(this::hasRequestMappingMethod)
+            .flatMap(method -> createHandlerExecutions(instance, method))
+            .forEach(this::putHandlerExecution);
+    }
+
+    private boolean hasRequestMappingMethod(Method method) {
+        return method.isAnnotationPresent(RequestMapping.class);
+    }
+
+    private Stream<Map.Entry<HandlerKey, HandlerExecution>> createHandlerExecutions(Object instance, Method method) {
+        HandlerExecution handlerExecution = makeHandlerExecution(instance, method);
+        return makeHandlerKeys(handlerExecution).entrySet().stream();
+    }
+
+    private void putHandlerExecution(Map.Entry<HandlerKey, HandlerExecution> entry) {
+        handlerExecutions.put(entry.getKey(), entry.getValue());
+    }
+
+    private HandlerExecution makeHandlerExecution(Object instance, Method method) {
+        return new HandlerExecution(argumentResolvers, instance, method);
+    }
+
+    private Map<HandlerKey, HandlerExecution> makeHandlerKeys(final HandlerExecution handlerExecution) {
+        RequestMapping requestMapping = handlerExecution.getMethod().getAnnotation(RequestMapping.class);
+        return Stream.of(requestMapping.method())
+            .collect(Collectors.toMap(
+                requestMethod -> new HandlerKey(requestMapping.value(), requestMethod),
+                requestMethod -> handlerExecution));
     }
 }
