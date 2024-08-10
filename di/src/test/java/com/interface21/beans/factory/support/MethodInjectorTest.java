875947086c936b3601b32d89fb11dff939ec8259
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-10 12:09:43 +0900

    test : 테스트 동작하도록 수정

--- di/src/test/java/com/interface21/beans/factory/support/MethodInjectorTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/MethodInjectorTest.java
@@ -3,6 +3,7 @@ package com.interface21.beans.factory.support;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import samples.JdbcSampleRepository;
 import samples.SampleFieldService;
 import samples.SampleMethodService;
@@ -15,16 +16,17 @@ class MethodInjectorTest {
     class Inject {
 
         @DisplayName("BeanDefinition과 BeanFactory를 주입받아 BeanDefinition에 등록된 Method를 통해 Bean을 생성한다")
+        @Test
         void inject() {
             // given
             final var beanFactory = new DefaultListableBeanFactory();
-            final var fieldInjector = new FieldInjector();
+            final var methodInjector = new MethodInjector();
 
             beanFactory.registerBeanDefinition(JdbcSampleRepository.class, new GenericBeanDefinition(JdbcSampleRepository.class));
             beanFactory.registerBeanDefinition(SampleFieldService.class, new GenericBeanDefinition(SampleMethodService.class));
 
             // when
-            final var sampleMethodService = (SampleMethodService) fieldInjector.inject(new GenericBeanDefinition(SampleMethodService.class), beanFactory);
+            final var sampleMethodService = (SampleMethodService) methodInjector.inject(new GenericBeanDefinition(SampleMethodService.class), beanFactory);
 
             // then
             assertNotNull(sampleMethodService);
