2570911324bc570638d48f68aa17b31f534614ca
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-12 14:12:30 +0900

    no message

--- di/src/main/java/com/interface21/context/support/AnnotationConfigWebApplicationContext.java
+++ di/src/main/java/com/interface21/context/support/AnnotationConfigWebApplicationContext.java
@@ -1,15 +1,12 @@
 package com.interface21.context.support;
 
+import com.interface21.beans.factory.support.BeanFactoryUtils;
 import com.interface21.beans.factory.support.BeanScanner;
 import com.interface21.beans.factory.support.DefaultListableBeanFactory;
 import com.interface21.context.ApplicationContext;
-import com.interface21.context.annotation.ComponentScan;
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class AnnotationConfigWebApplicationContext implements ApplicationContext {
 
@@ -20,7 +17,7 @@ public class AnnotationConfigWebApplicationContext implements ApplicationContext
         AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(beanFactory);
         reader.loadBeanDefinitions(configurationClasses);
         final var scanner = new BeanScanner(beanFactory);
-        scanner.scan(getBasePackages(configurationClasses));
+        scanner.scan(BeanFactoryUtils.getBasePackages(configurationClasses));
         beanFactory.initialize();
     }
 
@@ -37,30 +34,5 @@ public class AnnotationConfigWebApplicationContext implements ApplicationContext
     @Override
     public Map<Class<?>, Object> getBeansWithAnnotation(Class<? extends Annotation> annotationType) {
         return beanFactory.getBeansWithAnnotation(annotationType);
-    }
-
-    private String[] getBasePackages(Class<?>... configurationClasses) {
-        return Arrays.stream(configurationClasses)
-            .map(this::getBasePackages)
-            .flatMap(List::stream)
-            .distinct()
-            .toArray(String[]::new);
-    }
-
-    private List<String> getBasePackages(Class<?> configurationClass) {
-        if (!configurationClass.isAnnotationPresent(ComponentScan.class)) {
-            return List.of();
-        }
-
-        ComponentScan componentScan = configurationClass.getAnnotation(ComponentScan.class);
-        String[] values = componentScan.value();
-        String[] basePackages = componentScan.basePackages();
-
-        if (values.length == 0 && basePackages.length == 0) {
-            return List.of(configurationClass.getPackageName());
-        }
-
-        return Stream.concat(Arrays.stream(values), Arrays.stream(basePackages))
-            .toList();
     }
 }
