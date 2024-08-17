1251e8942261ad13b60be2a23e2b66d3c8a38015
Jivebreaddev <jivebreaddev@gmail.com>
2024-08-17 13:36:33 +0900

    style(format): formatting 하였습니다.

--- di/src/main/java/com/interface21/beans/factory/support/BeanDefinitionRegistry.java
+++ di/src/main/java/com/interface21/beans/factory/support/BeanDefinitionRegistry.java
@@ -3,5 +3,6 @@ package com.interface21.beans.factory.support;
 import com.interface21.beans.factory.config.BeanDefinition;
 
 public interface BeanDefinitionRegistry {
+
     void registerBeanDefinition(Class<?> clazz, BeanDefinition beanDefinition);
 }
