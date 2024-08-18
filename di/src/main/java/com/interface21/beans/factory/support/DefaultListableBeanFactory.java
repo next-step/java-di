4349f581a9d398f202d6da5aea6e7cb5c32855ff
youngbinary <youngbin@barogo.com>
2024-08-18 21:27:46 +0900

    feat(AnnotationHandlerMapping): MVC 프레임 워크의 AnnotationHandlerMapping 이 BeanFactory 와 BeanScanner 를 활용해 동작하도록 한다

--- di/src/main/java/com/interface21/beans/factory/support/DefaultListableBeanFactory.java
+++ di/src/main/java/com/interface21/beans/factory/support/DefaultListableBeanFactory.java
@@ -5,9 +5,11 @@ import com.interface21.beans.BeanUtils;
 import com.interface21.beans.factory.BeanFactory;
 import com.interface21.beans.factory.config.BeanDefinition;
 import com.interface21.beans.factory.config.GenericBeanDefinition;
+import com.interface21.context.stereotype.Controller;
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import org.apache.commons.lang3.ObjectUtils;
@@ -31,6 +33,22 @@ public class DefaultListableBeanFactory implements BeanFactory {
   @Override
   public Set<Class<?>> getBeanClasses() {
     return beanDefinitionMap.keySet();
+  }
+
+  public Map<Class<?>, Object> getControllers() {
+    Map<Class<?>, Object> controllers = new HashMap<>();
+    for (Map.Entry<Class<?>, Object> map : singletonObjects.entrySet()) {
+      findController(map, controllers);
+    }
+
+    return controllers;
+  }
+
+  private void findController(Entry<Class<?>, Object> map, Map<Class<?>, Object> controllers) {
+    final Class<?> clazz = map.getKey();
+    if (clazz.isAnnotationPresent(Controller.class)) {
+      controllers.put(clazz, map.getValue());
+    }
   }
 
   @Override
@@ -85,9 +103,9 @@ public class DefaultListableBeanFactory implements BeanFactory {
   private Object[] findArguments(Class<?>[] parameterTypes) {
     Object[] arguments = new Object[parameterTypes.length];
 
-    for(int i = 0; i < parameterTypes.length; i++) {
+    for (int i = 0; i < parameterTypes.length; i++) {
       Optional<Class<?>> implClass = BeanFactoryUtils.findConcreteClass(parameterTypes[i], getBeanClasses());
-      if(implClass.isEmpty()) {
+      if (implClass.isEmpty()) {
         throw new NoSuchBeanDefinitionException("Could not find concrete class for bean class");
       }
       arguments[i] = this.getBean(implClass.get());
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,30 +1,90 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.web.method.support.HandlerMethodArgumentResolver;
 import com.interface21.webmvc.servlet.mvc.HandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.support.HttpRequestArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.HttpResponseArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.ModelArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.PathVariableArgumentResolver;
+import com.interface21.webmvc.servlet.mvc.tobe.support.RequestParamArgumentResolver;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AnnotationHandlerMapping implements HandlerMapping {
+
+    private static final List<HandlerMethodArgumentResolver> argumentResolvers = List.of(
+        new HttpRequestArgumentResolver(),
+        new HttpResponseArgumentResolver(),
+        new RequestParamArgumentResolver(),
+        new PathVariableArgumentResolver(),
+        new ModelArgumentResolver()
+    );
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final Map<Class<?>, Object> controllers;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Map<Class<?>, Object> controllers) {
         this.handlerExecutions = new HashMap<>();
+        this.controllers = controllers;
     }
 
     public void initialize() {
-        final var controllerScanner = new ControllerScanner();
-        handlerExecutions.putAll(controllerScanner.scan(basePackage));
+        handlerExecutions.putAll(this.addHandlerExecutions(controllers));
         log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private Map<HandlerKey, HandlerExecution> addHandlerExecutions(Map<Class<?>, Object> controllers) {
+        final var handlers = new HashMap<HandlerKey, HandlerExecution>();
+        for (Map.Entry<Class<?>, Object> map : controllers.entrySet()) {
+            Class<?> clazz = map.getKey();
+            addHandlerExecution(handlers, map.getValue(), clazz.getMethods());
+        }
+        return handlers;
+    }
+
+    private void addHandlerExecution(
+        final Map<HandlerKey, HandlerExecution> handlerExecutions,
+        final Object target,
+        final Method[] methods
+    ) {
+        Arrays.stream(methods)
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .forEach(method -> {
+                final var requestMapping = method.getAnnotation(RequestMapping.class);
+                handlerExecutions.putAll(createHandlerExecutions(target, method, requestMapping));
+                log.info("register handlerExecution : url is {}, request method : {}, method is {}",
+                    requestMapping.value(), requestMapping.method(), method);
+            });
+    }
+
+    private Map<HandlerKey, HandlerExecution> createHandlerExecutions(final Object target, final Method method, final RequestMapping requestMapping) {
+        return mapHandlerKeys(requestMapping.value(), requestMapping.method())
+            .stream()
+            .collect(Collectors.toMap(
+                handlerKey -> handlerKey,
+                handlerKey -> new HandlerExecution(argumentResolvers, target, method)
+            ));
+    }
+
+    private List<HandlerKey> mapHandlerKeys(final String value, final RequestMethod[] originalMethods) {
+        var targetMethods = originalMethods;
+        if (targetMethods.length == 0) {
+            targetMethods = RequestMethod.values();
+        }
+        return Arrays.stream(targetMethods)
+            .map(method -> new HandlerKey(value, method))
+            .collect(Collectors.toList());
     }
 
     public Object getHandler(final HttpServletRequest request) {
