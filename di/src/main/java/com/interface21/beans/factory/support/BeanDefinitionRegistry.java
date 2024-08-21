f1310648eb3fb0aa676302cbeed8d884789f33c3
youngbinary <youngbin@barogo.com>
2024-08-21 20:58:35 +0900

    test: @Bean 어노테이션이 붙어있는 메서드 리스트를 반환한다

--- di/src/main/java/com/interface21/beans/factory/support/BeanDefinitionRegistry.java
+++ di/src/main/java/com/interface21/beans/factory/support/BeanDefinitionRegistry.java
@@ -1,7 +1,13 @@
 package com.interface21.beans.factory.support;
 
 import com.interface21.beans.factory.config.BeanDefinition;
+import java.util.Set;
 
 public interface BeanDefinitionRegistry {
-    void registerBeanDefinition(Class<?> clazz, BeanDefinition beanDefinition);
+
+    void register(Class<?> clazz, BeanDefinition beanDefinition);
+
+    Set<Class<?>> getBeanClasses();
+
+    BeanDefinition get(Class<?> clazz);
 }
