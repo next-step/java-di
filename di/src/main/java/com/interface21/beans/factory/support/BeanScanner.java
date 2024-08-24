90bf5bc3a0381b25f33e2389e034b2404bd5f85a
youngbinary <youngbin@barogo.com>
2024-08-24 20:21:44 +0900

    feat: BeanScanner 에서 빈들을 스캔하고 BeanDefinition 객체로 리턴할 수 있도록 한다

--- di/src/main/java/com/interface21/beans/factory/support/BeanScanner.java
+++ di/src/main/java/com/interface21/beans/factory/support/BeanScanner.java
@@ -1,10 +1,14 @@
 package com.interface21.beans.factory.support;
 
+import com.interface21.beans.factory.config.BeanDefinition;
+import com.interface21.beans.factory.config.GenericBeanDefinition;
+import com.interface21.context.annotation.Configuration;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.context.stereotype.Repository;
 import com.interface21.context.stereotype.Service;
 import java.lang.annotation.Annotation;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.reflections.Reflections;
@@ -14,45 +18,15 @@ public final class BeanScanner {
   private final Reflections reflections;
   private final List<Class<? extends Annotation>> beanAnnotations;
 
-  public BeanScanner(String... basePackages) {
+  public BeanScanner(List<String> basePackages) {
     this.reflections = new Reflections(basePackages);
-    this.beanAnnotations = List.of(Controller.class, Service.class, Repository.class);
+    this.beanAnnotations = List.of(Controller.class, Service.class, Repository.class, Configuration.class);
   }
 
-  public Set<Class<?>> scan() {
+  public Map<Class<?>, BeanDefinition> scan() {
     return beanAnnotations.stream()
         .map(reflections::getTypesAnnotatedWith)
         .flatMap(Set::stream)
-        .collect(Collectors.toSet());
+        .collect(Collectors.toMap(clazz -> clazz, GenericBeanDefinition::from));
   }
-
-
-  // TODO 로 진행하겠습니다
-//  public Set<Class<?>> scan() {
-//    Set<Class<?>> result = new HashSet<>();
-//
-//    for (Class<? extends Annotation> annotation : beanAnnotations) {
-//      Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(annotation);
-//
-//      for (Class<?> clazz : annotatedClasses) {
-//
-//        if (clazz.isAnnotationPresent(Configuration.class)) {
-//          collectBeanMethodReturnTypes(clazz, result);
-//        } else {
-//          result.add(clazz);
-//        }
-//      }
-//    }
-//
-//    return result;
-//  }
-
-  // TODO 로 진행하겠습니다
-//  private void collectBeanMethodReturnTypes(Class<?> configurationClass, Set<Class<?>> result) {
-//    Set<Method> beanMethods = BeanFactoryUtils.getBeanMethods(configurationClass, Bean.class);
-//
-//    for (Method method : beanMethods) {
-//      result.add(method.getReturnType());
-//    }
-//  }
 }
--- di/src/test/java/com/interface21/beans/factory/support/BeanScannerTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/BeanScannerTest.java
@@ -3,7 +3,9 @@ package com.interface21.beans.factory.support;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.Set;
+import com.interface21.beans.factory.config.BeanDefinition;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import samples.JdbcSampleRepository;
@@ -15,16 +17,17 @@ class BeanScannerTest {
   @Test
   @DisplayName("@Repository, @Service, @Controller 어노테이션이 존재하는 모든 클래스를 스캔한다")
   void scanTest() {
-    BeanScanner scanner = new BeanScanner("samples");
+    List<String> basePackages = List.of("samples");
+    BeanScanner scanner = new BeanScanner(basePackages);
 
-    Set<Class<?>> result = scanner.scan();
+    Map<Class<?>, BeanDefinition> result = scanner.scan();
 
     assertAll(
         () -> assertThat(result).isNotNull(),
         () -> assertThat(result).isNotEmpty(),
-        () -> assertThat(result.contains(JdbcSampleRepository.class)).isTrue(),
-        () -> assertThat(result.contains(SampleController.class)).isTrue(),
-        () -> assertThat(result.contains(SampleService.class)).isTrue()
+        () -> assertThat(result.containsKey(JdbcSampleRepository.class)).isTrue(),
+        () -> assertThat(result.containsKey(SampleController.class)).isTrue(),
+        () -> assertThat(result.containsKey(SampleService.class)).isTrue()
     );
   }
 
