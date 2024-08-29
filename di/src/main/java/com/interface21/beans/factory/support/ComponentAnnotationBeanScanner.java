1b5fcae0cd74d6d8d053954a0276b76a9d8d6b75
jeongho <qkr2435@gmail.com>
2024-08-30 00:36:42 +0900

    feat: beanScanner 역할 분리

---
+++ di/src/main/java/com/interface21/beans/factory/support/ComponentAnnotationBeanScanner.java
@@ -0,0 +1,22 @@
+package com.interface21.beans.factory.support;
+
+import com.interface21.context.stereotype.Component;
+import org.reflections.Reflections;
+
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ComponentAnnotationBeanScanner extends BeanScanner {
+
+  public ComponentAnnotationBeanScanner(String... basePackages) {
+    super(basePackages);
+  }
+
+  public Set<Class<?>> getComponentAnnotationClasses() {
+    Reflections reflections = createReflections();
+    return reflections.getTypesAnnotatedWith(Component.class).stream()
+        .filter(Predicate.not(Class::isAnnotation))
+        .collect(Collectors.toSet());
+  }
+}
