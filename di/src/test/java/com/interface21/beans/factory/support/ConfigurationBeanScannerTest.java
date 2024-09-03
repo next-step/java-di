c0daa1cb80f095b28980c76a28e7b47220c9117f
jeongho <qkr2435@gmail.com>
2024-09-03 17:52:57 +0900

    test: ComponentAnnotationBeanScannerTest 테스트 케이스 추가

--- di/src/test/java/com/interface21/beans/factory/support/ConfigurationBeanScannerTest.java
+++ di/src/test/java/com/interface21/beans/factory/support/ConfigurationBeanScannerTest.java
@@ -6,22 +6,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import samples.ExampleConfig;
 import samples.ExampleFailConfig;
 import samples.JdbcSampleRepository;
 
 class ConfigurationBeanScannerTest {
-  private ConfigurationBeanScanner scanner;
-
-  @BeforeEach
-  void initilaize() {
-    scanner = new ConfigurationBeanScanner("samples");
-  }
 
   @Test
   void Bean_어노테이션이_있는_메소드를_포함한_Configuration_클래스만_반환해야_한다() {
+
+    ConfigurationBeanScanner scanner = new ConfigurationBeanScanner("samples");
     Set<Class<?>> configClasses = scanner.scanForBeans();
 
     assertNotNull(configClasses);
@@ -31,6 +26,8 @@ class ConfigurationBeanScannerTest {
 
   @Test
   void Bean_어노테이션이_없는_메소드만_있는_Configuration_클래스는_반환하지_않아야_한다() {
+
+    ConfigurationBeanScanner scanner = new ConfigurationBeanScanner("samples");
     Set<Class<?>> configClasses = scanner.scanForBeans();
 
     assertFalse(configClasses.contains(ExampleFailConfig.class));
@@ -38,6 +35,8 @@ class ConfigurationBeanScannerTest {
 
   @Test
   void Configuration_어노테이션이_없는_클래스는_반환하지_않아야_한다() {
+
+    ConfigurationBeanScanner scanner = new ConfigurationBeanScanner("samples");
     Set<Class<?>> configClasses = scanner.scanForBeans();
 
     assertFalse(configClasses.contains(ExampleFailConfig.class));
@@ -45,11 +44,13 @@ class ConfigurationBeanScannerTest {
 
   @Test
   void 빈_패키지를_처리할_수_있어야_한다() {
-    ConfigurationBeanScanner emptyScanner = new ConfigurationBeanScanner("empty");
+
+    ConfigurationBeanScanner emptyScanner = new ConfigurationBeanScanner("empty.samples");
     Set<Class<?>> configClasses = emptyScanner.scanForBeans();
 
     assertTrue(configClasses.isEmpty());
   }
+
   @Test
   void 빈_팩토리가_샘플_패키지의_빈들을_올바르게_생성하고_주입해야_한다() {
 
@@ -62,7 +63,6 @@ class ConfigurationBeanScannerTest {
         .flatMap(scanner -> scanner.scanForBeans().stream())
         .collect(Collectors.toSet());
 
-    // JdbcSampleRepository 빈 검증
     assertNotNull(beanClasses);
     assertTrue(beanClasses.contains(JdbcSampleRepository.class));
 
